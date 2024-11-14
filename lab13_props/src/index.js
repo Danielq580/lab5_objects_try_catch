a899137ade3347ee696ba4372640903248b444c5
DanielQ <143388488+Danielq580@users.noreply.github.com>
2024-11-14 21:35:50 +0000

    lab13

--- lab13_props/src/index.js
+++ lab13_props/src/index.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import User from './comments'
+
 // import all images
 import avataruser1 from './images/batman.png'
 import avataruser2 from './images/avatar2.png'
 import avataruser3 from './images/avatar3.png'
+
+//import JS files
+import User_feedback from './user_feedback';
+import './index.css';
+import User from './comments'
 
 const App = function(){
   return(
     <>
     <h1 style={{textAlign:"center"}}>Daniel Quan</h1>
     <section className='container'>
-      <User image={avataruser1} name="Batman" date= "11/12/24" msg="I am Batman"/>
-      <User image={avataruser2} name="Peter" date= "11/12/24" msg="Need more rest!"/>
-      <User image={avataruser3} name="Jessica" date= "11/12/24" msg="Great job!"/>
-    </section>
+    
+      {/* User 1 */}
+    <User_feedback username="Batman"><User image={avataruser1} date= "11/12/24" msg="I am Batman"/></User_feedback>
+
+      {/* User 2 */}
+      <User_feedback username="Peter"> <User image={avataruser2} date= "11/12/24" msg="Need more rest!"/></User_feedback>
+
+       {/* User 3 */}
+       <User_feedback username="Jessica"><User image={avataruser3} date= "11/12/24" msg="Great job!"/> </User_feedback>
+      </section>
+
     </>
   )
 }
