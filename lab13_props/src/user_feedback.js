36a98edb347d001e618c98c156bd6d2850e605ef
DanielQ <143388488+Danielq580@users.noreply.github.com>
2024-11-22 22:21:15 +0000

    part 2 props

--- lab13_props/src/user_feedback.js
+++ lab13_props/src/user_feedback.js
@@ -1,8 +1,14 @@
 import React from "react";
 import './index'
 import './index.css'
+import Modalwindow from "./modalwindow";
 
 const User_feedback = function(props){
+    // open modal window
+    const openmodalwindow = () => {
+        const modalwindow = document.querySelector(".modalwindow")
+        modalwindow.style.display = "block";
+    }
     return(
         <>
             <div className="feedbackcontainer">
@@ -10,10 +16,20 @@ const User_feedback = function(props){
                     <div className="content"><p>{props.username}</p> </div>
                     <div className="description">{props.children}</div>
                     <div className="cardfooter">
-                        <p className="addicon"><span>&#10011;</span>Add feedback</p> 
+                        <p className="addicon" onClick={openmodalwindow}><span>&#10011;</span>Add feedback</p> 
                         </div>
                 </section>
             </div>
+
+
+            {/*post comments */}
+            <section className="postcomments">
+                <ul className="commentlist">
+
+                </ul>
+            </section>
+            {/* MOMAL WINDOW */}
+            <Modalwindow />
         </>
     )
 }
